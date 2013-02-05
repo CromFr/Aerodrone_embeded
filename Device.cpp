389ea22b3fd7b01eb598bdee68dd16fc43698518
Crom <crom@hptcharl15.(none)>
2013-02-05 08:58:53 +0100

    Critical error routine restaured

--- Device.cpp
+++ Device.cpp
@@ -133,9 +133,7 @@ void Device::OnErrorRoutine()
 
 void Device::OnCriticalErrorRoutine()
 {
-    //m_stabctrl->LandRoutine(5);
-    m_stabctrl->Stop(true);
-    m_mot->Stop(true);
+    m_stabctrl->LandRoutine(5);
 
 	//Bip the "error" tune
 	int nSeq[] = {200,400,200,400,200,400,200,400,200,400,200,200};
